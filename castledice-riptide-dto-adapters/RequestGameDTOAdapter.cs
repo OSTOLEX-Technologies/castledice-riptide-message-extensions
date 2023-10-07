d43faec565ded93e17d0c7fb3dfb7206817188a8
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-07 20:10:48 +0300

    Added dependencies and stub for RequestGameDTOAdapter.

---
+++ castledice-riptide-dto-adapters/RequestGameDTOAdapter.cs
@@ -0,0 +1,16 @@
+﻿using Riptide;
+
+namespace castledice_riptide_dto_adapters;
+
+public class RequestGameDTOAdapter : IMessageSerializable
+{
+    public void Serialize(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Deserialize(Message message)
+    {
+        throw new NotImplementedException();
+    }
+}
