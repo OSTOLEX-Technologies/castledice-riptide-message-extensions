9d5cb3649b6781befa913b04420d907da2322f6c
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-08 20:22:36 +0300

    Finished adapter for CreateGameDTO.

---
+++ castledice-riptide-dto-adapters-tests/CreateGameDTOAdapterTests.cs
@@ -0,0 +1,34 @@
+﻿using casltedice_events_logic.ServerToClient;
+using castledice_riptide_dto_adapters;
+using static castledice_riptide_dto_adapters_tests.ObjectCreationUtility;
+
+namespace castledice_riptide_dto_adapters_tests;
+
+public class CreateGameDTOAdapterTests
+{
+    [Fact]
+    public void Serialize_ShouldThrowArgumentException_IfDTOIsNull()
+    {
+        var adapter = new CreateGameDTOAdapter();
+        var message = GetEmptyMessage();
+        
+        Assert.Throws<ArgumentException>(() => adapter.Serialize(message));
+    }
+    
+    [Fact]
+    public void DeserializingDTOFromMessage_ShouldGiveDTOWithSameProperties()
+    {
+        var DTOToSend = new CreateGameDTO(GetGameStartData());
+        var message = GetEmptyMessage();
+        var sentAdapter = new CreateGameDTOAdapter()
+        {
+            DTO = DTOToSend
+        };
+        
+        message.AddSerializable(sentAdapter);
+        var receivedAdapter = message.GetSerializable<CreateGameDTOAdapter>();
+        var receivedDTO = receivedAdapter.DTO;
+        
+        Assert.Equal(DTOToSend.GameStartData, receivedDTO.GameStartData);
+    }
+}
---
+++ castledice-riptide-dto-adapters/CreateGameDTOAdapter.cs
@@ -0,0 +1,25 @@
+﻿using casltedice_events_logic.ServerToClient;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+
+namespace castledice_riptide_dto_adapters;
+
+public class CreateGameDTOAdapter : IMessageSerializable
+{
+    public CreateGameDTO? DTO { get; set; }
+
+    public void Serialize(Message message)
+    {
+        if (DTO is null)
+        {
+            throw new ArgumentException("Cannot serialize DTO because it is null.");
+        }
+        message.AddGameStartData(DTO.GameStartData);
+    }
+
+    public void Deserialize(Message message)
+    {
+        var gameStartData = message.GetGameStartData();
+        DTO = new CreateGameDTO(gameStartData);
+    }
+}
